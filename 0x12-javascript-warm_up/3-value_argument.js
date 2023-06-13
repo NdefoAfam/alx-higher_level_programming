591690efd37d1d5f7ac672ab93cf02a7e97ed8cd
NdefoAfam <ndefoafam@gmail.com>
2023-06-13 08:55:00 -0700

    updated

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-const firstArgument = args[0];
-
-if (firstArgument) {
-  console.log(firstArgument);
-} else {
-  console.log("No argument");
-}
+#!/usr/bin/node
+console.log(process.argv[2] ? process.argv[2] : 'No argument');
