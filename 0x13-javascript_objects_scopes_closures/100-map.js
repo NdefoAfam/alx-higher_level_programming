5b309da20aafc3b77277c7b8c6895d31990fc742
NdefoAfam <ndefoafam@gmail.com>
2023-06-15 01:25:25 -0700

    updated

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-const newList = list.map(function (num, index) {
-  return num * index;
-});
 
+const list = require('./100-data');
+
+const newList = list.map((value, index) => value * index);
+
+console.log('Initial list:');
 console.log(list);
+
+console.log('New list:');
 console.log(newList);
