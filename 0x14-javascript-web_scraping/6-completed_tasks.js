35a4a6fff48560756a40d33819814fd0f70d1a15
Ndefo Afam <ndefoafam@gmail.com>
2023-07-26 09:06:45 +0100

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,38 +1,22 @@
 #!/usr/bin/node
 const request = require('request');
 
-// Function to compute the number of completed tasks by user id
-function computeCompletedTasks(apiUrl) {
-  request(apiUrl, (error, response, body) => {
+if (process.argv.length > 2) {
+  request(process.argv[2], (error, response, responseBody) => {
+    const userCompletionCount = {};
+
     if (error) {
-      console.error('Error:', error);
-    } else if (response.statusCode !== 200) {
-      console.error('Invalid response:', response.statusCode);
-    } else {
-      const tasks = JSON.parse(body);
-
-      // Create an object to store the count of completed tasks for each user id
-      const completedTasksByUser = {};
-
-      // Loop through the tasks and count the completed tasks for each user
-      tasks.forEach((task) => {
-        if (task.completed) {
-          if (completedTasksByUser.hasOwnProperty(task.userId)) {
-            completedTasksByUser[task.userId]++;
-          } else {
-            completedTasksByUser[task.userId] = 1;
-          }
-        }
-      });
-
-      // Print the result
-      console.log(completedTasksByUser);
+      console.log(error);
     }
+
+    JSON.parse(responseBody).forEach(item => {
+      if (item.completed) {
+        if (!userCompletionCount[item.userId]) {
+          userCompletionCount[item.userId] = 0;
+        }
+        userCompletionCount[item.userId]++;
+      }
+    });
+    console.log(userCompletionCount);
   });
 }
-
-// Get the API URL from the command line argument
-const apiUrl = process.argv[2];
-
-// Call the function with the provided API URL
-computeCompletedTasks(apiUrl);
