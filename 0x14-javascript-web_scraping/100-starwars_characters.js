d52ef57e512f7287d3d80cfcaf76f10163e932f1
Ndefo Afam <ndefoafam@gmail.com>
2023-07-26 09:07:41 +0100

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,26 +1,23 @@
 #!/usr/bin/node
 
 const request = require('request');
+const BASE_URL = 'https://swapi-api.hbtn.io/api';
 
-const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
+if (process.argv.length > 2) {
+  request(`${BASE_URL}/films/${process.argv[2]}/`, (error, response, responseBody) => {
+    if (error) {
+      console.log(error);
+    }
+    const charactersURLs = JSON.parse(responseBody).characters;
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-
-  const data = JSON.parse(body);
-  const characters = data.characters;
-  for (const character of characters) {
-    request(character, (error, response, body) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
+    charactersURLs.forEach(characterURL => {
+      request(characterURL, (err, res, body) => {
+        if (err) {
+          console.log(err);
+        }
+        const characterName = JSON.parse(body).name;
+        console.log(characterName);
+      });
     });
-  }
-});
+  });
+}
